dd1fc25212c7ae83d9f3dfb8cae13dbde759c855
jverea <jvereagovan2018@gmail.com>
2019-05-06 12:12:56 -0500

    update

---
+++ src/CondoSales.java
@@ -0,0 +1,10 @@
+public class CondoSales {
+
+    public static void main(String[] args){
+        int userEntry1;
+        int userEntry2;
+        System.out.println("Do you want a 1 car garage or a 2 car parking space?");
+
+
+    }
+}
